26cdf66fff7178e2ce480dcb4c0fcd2e601b0cd8
Pauli Kaila <870219+pkaila@users.noreply.github.com>
2022-05-25 12:55:24 +0300

    Updated rx examples for rx v3

--- examples/reactive_extensions.py
+++ examples/reactive_extensions.py
@@ -2,9 +2,8 @@
 RuuviTagReactive and Reactive Extensions Subject examples
 """
 
-# TODO: Migrate Rx to v3
-
 from ruuvitag_sensor.ruuvi_rx import RuuviTagReactive
+from rx import operators as ops
 
 tags = {
     'F4:A5:74:89:16:57': 'sauna',
@@ -19,39 +18,37 @@ ruuvi_rx.get_subject().\
     subscribe(print)
 
 # Get updates only for F4:A5:74:89:16:57
-ruuvi_rx.get_subject().\
-    filter(lambda x: x[0] == 'F4:A5:74:89:16:57').\
-    subscribe(lambda x: print(x[1]))
+ruuvi_rx.get_subject().pipe(
+    ops.filter(lambda x: x[0] == 'F4:A5:74:89:16:57')
+).subscribe(lambda x: print(x[1]))
 
 # Print only last updated every 10 seconds for F4:A5:74:89:16:57
-ruuvi_rx.get_subject().\
-    filter(lambda x: x[0] == 'F4:A5:74:89:16:57').\
-    sample(10000).\
-    subscribe(lambda data: print(data))
+ruuvi_rx.get_subject().pipe(
+    ops.filter(lambda x: x[0] == 'F4:A5:74:89:16:57'),
+    ops.sample(10.0)
+).subscribe(lambda data: print(data))
 
-# Print only last updated every 10 seconds for every foud sensor
-ruuvi_rx.get_subject().\
-    group_by(lambda x: x[0]).\
-    subscribe(lambda x: x.sample(10000).subscribe(print))
+# Print only last updated every 10 seconds for every found sensor
+ruuvi_rx.get_subject().pipe(
+    ops.group_by(lambda x: x[0])
+).subscribe(lambda x: x.pipe(ops.sample(10.0)).subscribe(print))
 
 # Print all from the last 10 seconds for F4:A5:74:89:16:57
-ruuvi_rx.get_subject().\
-    filter(lambda x: x[0] == 'F4:A5:74:89:16:57').\
-    buffer_with_time(10000).\
-    subscribe(lambda datas: print(datas))
+ruuvi_rx.get_subject().pipe(
+    ops.filter(lambda x: x[0] == 'F4:A5:74:89:16:57'),
+    ops.buffer_with_time(10.0)
+).subscribe(lambda datas: print(datas))
 
 # Execute subscribe only once for F4:A5:74:89:16:57
 # when temperature goes over 80 degrees
-ruuvi_rx.get_subject().\
-    filter(lambda x: x[0] == 'F4:A5:74:89:16:57').\
-    filter(lambda x: x[1]['temperature'] > 80).\
-    take(1).\
-    subscribe(lambda x: print(
-        'Sauna is ready! Temperature: {}'.format(x[1]['temperature'])))
+ruuvi_rx.get_subject().pipe(
+    ops.filter(lambda x: x[0] == 'F4:A5:74:89:16:57'),
+    ops.filter(lambda x: x[1]['temperature'] > 80),
+    ops.take(1)
+).subscribe(lambda x: print('Sauna is ready! Temperature: {}'.format(x[1]['temperature'])))
 
 # Execute only every time when pressure changes for F4:A5:74:89:16:57
-ruuvi_rx.get_subject().\
-    filter(lambda x: x[0] == 'F4:A5:74:89:16:57').\
-    distinct_until_changed(lambda x: x[1]['pressure']).\
-    subscribe(lambda x: print(
-        'Pressure changed: {}'.format(x[1]['pressure'])))
+ruuvi_rx.get_subject().pipe(
+    ops.filter(lambda x: x[0] == 'F4:A5:74:89:16:57'),
+    ops.distinct_until_changed(lambda x: x[1]['pressure']),
+).subscribe(lambda x: print('Pressure changed: {}'.format(x[1]['pressure'])))
